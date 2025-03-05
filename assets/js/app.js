634dcdcb55f7b368f73dc8c096529148d92b1a25
irisxg <irisgovaard@gmail.com>
2025-03-05 15:10:25 +0100

    5 maart update

---
+++ assets/js/app.js
@@ -0,0 +1,49 @@
+const cars = [
+    {
+        merk: 'Porsche',
+        model: '911',
+        prijs: 204900,
+        brandstof: 'Benzine',
+        kilometerstand: 16952,
+        kleur: 'Grijs',
+        image: 'image_url'
+    },
+    {
+        merk: 'Porsche',
+        model: 'Cayenne',
+        prijs: 167653,
+        brandstof: 'Plug-in hybride',
+        kilometerstand: 16406,
+        kleur: 'Zwart',
+        image: 'image_url'
+    },
+    {
+        merk: 'Porsche',
+        model: 'Cayenne',
+        prijs: 119900,
+        brandstof: 'Plug-in hybride',
+        kilometerstand: 22888,
+        kleur: 'Blauw',
+        image: 'image_url'
+    },
+    {
+        merk: 'Porsche',
+        model: '911',
+        prijs: 209950,
+        brandstof: 'Benzine',
+        kilometerstand: 5928,
+        kleur: 'grijs',
+        image: 'image_url'
+    },
+
+    {
+        merk: 'Porsche',
+        model: '718',
+        prijs: 134900,
+        brandstof: 'Benzine',
+        kilometerstand: 8319,
+        kleur: 'wit',
+        image: 'image_url'
+    },
+    
+];
